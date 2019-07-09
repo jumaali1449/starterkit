320012384e89f290fc8dd54f3206d4792af72db1
jumaali1449 <alijuma232@gmail.com>
2019-07-09 11:50:50 +0300

    ci testing

--- src/index.test.js
+++ src/index.test.js
@@ -13,7 +13,7 @@ describe('index.htm', ()=>{
 		const index = fs.readFileSync('./src/index.htm', 'utf-8');
 		const dom = new JSDOM(index);
 		const h1 = dom.window.document.getElementsByTagName('h1')[0];
-		expect(h1.innerHTML).to.equal('Congrats your express server has been configured correctly!');
+		expect(h1.innerHTML).to.equal('Congrats your express server has been configured correctly?');
 		dom.window.close();
 	});
 });
